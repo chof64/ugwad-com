0e757297731b5e37037442c0bb6510cec43e26b5
Chad Fernandez <contact@chadfernandez.me>
2023-05-10 01:53:12 +0000

    added layout and related components

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,49 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { mainNavigation } from "@/config/main-navigation";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+export default function Navigation() {
+  return (
+    <>
+      <header className="bg-white py-3">
+        <div className="m-auto flex w-svw-95 max-w-7xl justify-between">
+          <div className="my-auto h-full text-lg font-semibold">
+            UgwAd Party
+          </div>
+          <nav>
+            <NavigationMenu>
+              <NavigationMenuList>
+                {mainNavigation.primary.map((item) => (
+                  <NavigationMenuItem key={item.title}>
+                    <Link
+                      href={item.href}
+                      passHref
+                      target={item.external ? "_blank" : ""}
+                      rel={item.external ? "noreferrer noopener" : ""}
+                    >
+                      <NavigationMenuLink
+                        className={cn(
+                          navigationMenuTriggerStyle(),
+                          item.external ? "after:content-['_↗']" : ""
+                        )}
+                      >
+                        {item.title}
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                ))}
+              </NavigationMenuList>
+            </NavigationMenu>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+}
