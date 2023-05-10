c007c2ec6d61e6ea0fff08c20da882fe2f02bdd5
Chad Fernandez <contact@chadfernandez.me>
2023-05-10 01:52:23 +0000

    added navigation config file

---
+++ src/config/main-navigation.ts
@@ -0,0 +1,11 @@
+export const mainNavigation = {
+  primary: [
+    { title: "Home", href: "/" },
+    { title: "Profiles", href: "/profiles" },
+    {
+      title: "Facebook",
+      href: "https://facebook.com/UgwAdParty",
+      external: true,
+    },
+  ],
+};
