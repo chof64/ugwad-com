fd95abdaf4d778b0371ed157dcce75bba8206573
Chad Fernandez <contact@chadfernandez.me>
2023-05-10 21:06:49 +0000

    added site config and static data

---
+++ src/config/profiles-category.ts
@@ -0,0 +1,22 @@
+export const profilesCategory = {
+  positions: [
+    { position: "president", title: "President" },
+    { position: "vice_president", title: "Vice President" },
+    { position: "senator", title: "Senator" },
+    { position: "representative", title: "Representative" },
+    { position: "governor", title: "Governor" },
+    { position: "vice_governor", title: "Vice Governor" },
+    { position: "secretary", title: "Secretary" },
+    { position: "assistant_secretary", title: "Assistant Secretary" },
+    { position: "treasurer", title: "Treasurer" },
+    { position: "assistant_treasurer", title: "Assistant Treasurer" },
+    { position: "auditor", title: "Auditor" },
+    { position: "assistant_auditor", title: "Assistant Auditor" },
+    { position: "business_manager", title: "Business Manager" },
+    { position: "pro", title: "PRO" },
+    { position: "department_board", title: "Department Board" },
+    { position: "sergeant_at_arms", title: "Sergeant at Arms" },
+    { position: "muse", title: "Muse" },
+    { position: "sentinel", title: "Sentinel" },
+  ],
+};
---
+++ src/config/profiles-order.ts
@@ -0,0 +1,21 @@
+export const profilesOrder = {
+  upper: ["president", "vice_president", "senator"],
+  middle: ["representative"],
+  lower: [
+    "governor",
+    "vice_governor",
+    "secretary",
+    "assistant_secretary",
+    "treasurer",
+    "assistant_treasurer",
+    "auditor",
+    "assistant_auditor",
+    "business_manager",
+    "pro",
+    "department_board",
+    "sergeant_at_arms",
+    "muse",
+    "sentinel",
+  ],
+  departments: ["cas", "cba", "ccje", "ccs", "cea", "cot", "cte", "lhs"],
+};
